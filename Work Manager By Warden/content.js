9227d417957d689645c007f6c879d767310cf810
Cosmix <63625087+cosmixcom@users.noreply.github.com>
2022-07-16 09:09:46 -0700

    Add files via upload

---
+++ Work Manager By Warden/content.js
@@ -0,0 +1,27 @@
+// alert("Help")
+
+var block = ["https://example.com","https://roblox.com"];
+
+switch (window.location.hostname) {
+
+    case "sites.google.com":  
+
+    // history.back()
+    // window.open("https://sites.google.com/view/wardenblockedpage/work");
+
+    break;
+
+    case "serverguard.wixsite.com":  
+
+        
+
+    break;
+
+    default:
+
+        history.back()
+        window.open("https://sites.google.com/view/wardenblockedpage/work");
+    
+
+};
+
